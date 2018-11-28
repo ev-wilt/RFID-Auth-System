0637caa8de45dba887362b28c2ae81ec2c789235
Evan Wilt <evdawilt@umail.iu.edu>
2018-11-28 14:11:06 -0500

    Finished GET request

--- server/users.py
+++ server/users.py
@@ -1,20 +1,29 @@
 from coapthon.resources.resource import Resource
 import boto3
+import json
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('RFID-Users')
 
 class UsersResource(Resource):
     def __init__(self, name="BasicResource", coap_server=None):
         super(UsersResource, self).__init__(name, coap_server, visible=True,
                                             observable=True, allow_children=True)
-        self.payload = {}
+        self.payload = "test"
 
     def render_GET(self, request):
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('RFID-Users')
+        res = table.get_item(
+            Key = {
+                'UID': request.uri_query
+            }
+        )
+        try:
+            self.payload = json.dumps(res['Item'])
+        except KeyError:
+            self.payload = json.dumps({})
         return self
 
     def render_POST(self, request):
-        dynamodb = boto3.resource('dynamodb')
-        table = dynamodb.Table('RFID-Users')
         res = UsersResource()
         res.location_query = request.uri_query
         res.payload = request.payload
